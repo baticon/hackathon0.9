332262c00f63379338543f7eae759e103b06a0ba
Batyrbek Kantarbayev <batyrbek.kantarbayev@jits-3-17.jits.ipa.dc>
2022-07-09 13:52:11 +0600

    updated

---
+++ src/components/services/postFile.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import style from "../userMainPage/userMainPage.module.css";
+
+function FileUpload() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
+  };
+
+  const handleSubmission = async () => {
+    const formData = new FormData();
+    console.log("dataJson");
+    formData.append("file", selectedFile);
+    try {
+      const data = await fetch(
+        //   "https://freeimage.host/api/1/upload?key=6d207e02198a847aa98d0a2a901485a5",
+        `http://164.92.192.48:9095/upload/file/database?name=${selectedFile.name}`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      const dataJson = await data.json();
+      console.log(data);
+      console.log(dataJson);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div className={style.fileUpload}>
+      <input
+        className={style.fileInput}
+        type="file"
+        name="file"
+        onChange={changeHandler}
+      />
+      {isFilePicked ? (
+        <div>
+          <p className={style.paragraph}>
+            Наименование файла: {selectedFile.name}
+          </p>
+          <p className={style.paragraph}>Тип файла: {selectedFile.type}</p>
+          <p className={style.paragraph}>
+            Размер в байтах: {selectedFile.size}
+          </p>
+          <p className={style.paragraph}>
+            Последние изменение:{" "}
+            {selectedFile.lastModifiedDate.toLocaleDateString()}
+          </p>
+        </div>
+      ) : (
+        <p style={{ paddingTop: "5%" }}>Файл не прикреплен</p>
+      )}
+
+      <div>
+        <button
+          className={style.submitButton}
+          onClick={handleSubmission}
+          type="button"
+        >
+          Отправить
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default FileUpload;
--- src/components/userMainPage/userMainPage.js
+++ src/components/userMainPage/userMainPage.js
@@ -1,14 +1,44 @@
 import Header from "../header/header";
 import Footer from "../footer/footer";
+import PostFile from "../services/postFile";
+import question from "../media/question.png";
+import style from "./userMainPage.module.css";
+
+//https://www.pluralsight.com/guides/uploading-files-with-reactjs
 
 const UserMainPage = () => {
   return (
     <div>
       <Header />
-      <div>
-        <form>
-          <div>
-            <span>some form text</span>
+      <div className={style.container}>
+        <form className={style.formContainer}>
+          <div className={style.uploadContainer}>
+            <div className={style.fileUploadContainer} style={{}}>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <span>Вид на жительство или удостоверение</span>
+                <img
+                  src={question}
+                  className={style.hint}
+                  title="Вид на жительство иностранца в Республике Казахстан или
+                удостоверение лица без гражданства (для иностранцев и лиц без
+                гражданства, постоянно проживающих на территории Республики
+                Казахстан)"
+                ></img>
+              </div>
+              {PostFile()}
+            </div>
+            <div className={style.fileUploadContainer}>
+              <span>Пожалуйста загрузите свое удостоверение</span>
+              {PostFile()}
+            </div>
+            <div className={style.fileUploadContainer}>
+              <span>Пожалуйста загрузите справку ПКБ</span>
+              {PostFile()}
+            </div>
+            <div className={style.fileUploadContainer}>
+              <span>Пожалуйста загрузите медицинскую справку 086</span>
+              {PostFile()}
+            </div>
           </div>
         </form>
       </div>
