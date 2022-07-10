35eb58c8df35adca83007caafcbbd30c3f2891d9
Batyrbek Kantarbayev <batyrbek.kantarbayev@jits-3-17.jits.ipa.dc>
2022-07-10 10:15:32 +0600

    updated

--- src/components/userMainForm/userMainForm.js
+++ src/components/userMainForm/userMainForm.js
@@ -10,10 +10,10 @@ const UserMainForm = () => {
       <div>
         <form className={style.formContainer}>
           <div style={{ marginTop: "2%" }}>
-            <div className={style.inputContainerOne}>
-              <label className={style.labelOne}>ИИН</label>
+            <div className={style.IINContainer}>
+              <label className={style.IINlabel}>ИИН</label>
               <input
-                className={style.inputOne}
+                className={style.IINinput}
                 placeholder="например, 900101250050"
               ></input>
               <img
@@ -23,10 +23,10 @@ const UserMainForm = () => {
                 title="ИИН расположен на лицевой стороне удостоверения личности гражданина Республики Казахстан, ниже даты рождения в виде комбинации из 12-ти цифр, в паспорте гражданина Республики Казахстан ИИН указан на 2 странице.  Иностранцы и лица без гражданства, постоянно проживающие в Казахстане, у которых на лицевой стороне вида на жительство иностранца в Республике Казахстан ниже даты рождения или на 2 странице удостоверения лица без гражданства ИИН не указан, должны обратиться в органы внутренних дел по месту пребывания для переоформления документов."
               ></img>
             </div>
-            <div className={style.inputContainerOne}>
-              <label className={style.labelOne}>ФИО</label>
+            <div className={style.NAMEContainer}>
+              <label className={style.NAMElabel}>ФИО</label>
               <input
-                className={style.inputOne}
+                className={style.NAMEinput}
                 placeholder="например, Данов Руслан Багданович"
               ></input>
               <img
@@ -36,12 +36,12 @@ const UserMainForm = () => {
                 title="ФИО - если Вы сменили фамилию, укажите, пожалуйста, прежнюю"
               ></img>
             </div>
-            <div className={style.inputContainerOne}>
-              <label className={style.labelOne}>
+            <div className={style.DOBContainer}>
+              <label className={style.DOBlabel}>
                 Число, месяц и год рождения
               </label>
               <input
-                className={style.inputOne}
+                className={style.DOBinput}
                 placeholder="например, 01-01-1900"
               ></input>
               <img
@@ -51,10 +51,10 @@ const UserMainForm = () => {
                 title="Дата рождения в формате ЧЧ-ММ-ГГГГ"
               ></img>
             </div>
-            <div className={style.inputContainerOne}>
-              <label className={style.labelOne}>Место рождения</label>
+            <div className={style.POBContainer}>
+              <label className={style.POBlabel}>Место рождения</label>
               <input
-                className={style.inputOne}
+                className={style.POBinput}
                 placeholder="например, Казахстан, Астана"
               ></input>
               <img
@@ -64,10 +64,10 @@ const UserMainForm = () => {
                 title="Пожалуйста укажите страну и город (или село/деревню)"
               ></img>
             </div>
-            <div className={style.inputContainerOne}>
-              <label className={style.labelOne}>Национальность</label>
+            <div className={style.NATIONALITYContainer}>
+              <label className={style.NATIONALITYlabel}>Национальность</label>
               <input
-                className={style.inputOne}
+                className={style.NATIONALITYinput}
                 placeholder="например, казах"
               ></input>
               <img
@@ -77,10 +77,10 @@ const UserMainForm = () => {
                 title="Пожалуйста укажите национальность"
               ></img>
             </div>
-            <div className={style.inputContainerOne}>
-              <label className={style.labelOne}>Гражданство</label>
+            <div className={style.CITIZENSHIPContainer}>
+              <label className={style.CITIZENSHIPlabel}>Гражданство</label>
               <input
-                className={style.inputOne}
+                className={style.CITIZENSHIPinput}
                 placeholder="например, Казахстан"
               ></input>
               <img
@@ -90,25 +90,114 @@ const UserMainForm = () => {
                 title="Пожалуйста укажите гражданство"
               ></img>
             </div>
-            <div className={style.inputContainerOne}>
-              <label className={style.labelOne}>
+            <div className={style.IDCARDContainer}>
+              <label className={style.IDCARDlabel}>
                 Паспорт, удостоверение личности
               </label>
-              <input></input>
-              <input></input>
-              <input></input>
+              <div className={style.IDCARDContainerTwo}>
+                <input
+                  className={style.IDCARDinput}
+                  placeholder="например, N07080900"
+                ></input>
+                <label style={{ fontSize: "80%" }}>Серия</label>
+              </div>
+              <div className={style.inputContainerTwo}>
+                <input
+                  className={style.IDCARDinput}
+                  placeholder="например, 044332211"
+                ></input>
+                <label style={{ fontSize: "80%" }}>Номер</label>
+              </div>
+              <div className={style.inputContainerTwo}>
+                <input
+                  className={style.IDCARDinput}
+                  placeholder="например, МВД РК"
+                ></input>
+                <label style={{ fontSize: "80%" }}>Кем выдан</label>
+              </div>
               <img
                 src={question}
                 alt=""
                 className={style.hint}
-                title="Какая-нибудь подсказка"
+                title="1. Серия паспорта указана в паспорте гражданина РК и начинается с латинской буквы N. 2. Номер удостоверения указан на тыльной стороне удостоверения гражданина РК.  3. Орган выдачи удостоверения указан на передней части удостоверения гражданина РК"
+              ></img>
+            </div>
+            <div className={style.CONTACTSContainer}>
+              <label className={style.CONTACTSlabel}>Контактные данные</label>
+              <div className={style.CONTACTSContainerTwo}>
+                <div className={style.CONTACTSContainerThree}>
+                  <label className={style.CONTACTSlabelTwo}>
+                    Домашний телефон
+                  </label>
+                  <input
+                    className={style.CONTACTSinputTwo}
+                    placeholder="например, +7-7172-111-222"
+                  ></input>
+                </div>
+                <div className={style.CONTACTSContainerThree}>
+                  <label className={style.CONTACTSlabelTwo}>
+                    Рабочий телефон
+                  </label>
+                  <input
+                    className={style.CONTACTSinputTwo}
+                    placeholder="например, +7-777-111-2222"
+                  ></input>
+                </div>
+                <div className={style.CONTACTSContainerThree}>
+                  <label className={style.CONTACTSlabelTwo}>
+                    Мобильный телефон
+                  </label>
+                  <input
+                    className={style.CONTACTSinputTwo}
+                    placeholder="например, +7-777-333-4444"
+                  ></input>
+                </div>
+                <div className={style.CONTACTSContainerThree}>
+                  <label className={style.CONTACTSlabelTwo}>
+                    Адрес электронной почты
+                  </label>
+                  <input
+                    className={style.CONTACTSinputTwo}
+                    placeholder="например, jusan@gmail.com"
+                  ></input>
+                </div>
+                <div className={style.CONTACTSContainerThree}>
+                  <label className={style.CONTACTSlabelTwo}>
+                    Контактный - ФИО (родственника и/или знакомого)
+                  </label>
+                  <input
+                    className={style.CONTACTSinputTwo}
+                    placeholder="например, Баглан Данов"
+                  ></input>
+                </div>
+                <div className={style.CONTACTSContainerThree}>
+                  <label className={style.CONTACTSlabelTwo}>
+                    Контактный - номер телефона (родственника и/или знакомого)
+                  </label>
+                  <input
+                    className={style.CONTACTSinputTwo}
+                    placeholder="например, +7-777-555-6677"
+                  ></input>
+                </div>
+                <div className={style.CONTACTSContainerThree}>
+                  <label className={style.CONTACTSlabelTwo}>
+                    Контактный - степень родства (родственника и/или знакомого)
+                  </label>
+                  <input
+                    className={style.CONTACTSinputTwo}
+                    placeholder="например, отец"
+                  ></input>
+                </div>
+              </div>
+              <img
+                src={question}
+                alt=""
+                className={style.hint}
+                title="Пожалуйста укажите актуальные контактные данные по которым отдел кадров Jusan сможет установить контакт с Вами и Вашим ближайшим контактным лицом.  Контактные данные будут использоваться для рабочих и экстренных случаев"
               ></img>
             </div>
           </div>
-          <div>
-            <label></label>
-            <input></input>
-          </div>
+
           <div>
             <label></label>
             <input></input>
