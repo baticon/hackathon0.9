fd1f9846ea5fb65b07ff7bd2888e62c3047ff24f
Batyrbek Kantarbayev <batyrbek.kantarbayev@jits-3-17.jits.ipa.dc>
2022-07-11 16:03:58 +0600

    working menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,20 +3,25 @@ import exitIcon from "../media/exit.png";
 import style from "./header.module.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import menu from "./menu";
+import hamburger from "../media/hamburger.png";
+import { display } from "@mui/system";
+import FadeInOut from "./FadeInOut";
 
 const Header = () => {
   const navigate = useNavigate();
   const [bool, setBool] = useState(true); //placeholder for sessionstorage
 
-  function button() {
-    if (bool === true) {
+  const [toggle, setToggle] = useState(false);
+
+  function button(toggle) {
+    if (bool === true && toggle == true) {
       return (
         <button
           className={style.exitButton}
           onClick={() => {
             console.log("Exit condition");
-            window.localStorage.clear();
+            // window.localStorage.clear();
+            window.localStorage.removeItem("access_token");
             navigate("/login");
           }}
         >
@@ -25,7 +30,9 @@ const Header = () => {
         </button>
       );
     }
-    return null;
+    if (bool === true && toggle == false) {
+      return <button className={style.exitButtonHidden}>Выход</button>;
+    }
   }
 
   return (
@@ -36,15 +43,41 @@ const Header = () => {
       <a href="https://jusan.kz/">
         <img src={logo} className={style.logo} alt=""></img>
       </a>
-      <ol>
-        <a className={style.menu} href="https://jusan.kz/">
-          <li>Политика конфиденциальности</li>
-        </a>
-        <a className={style.menu} href="https://jusan.kz/">
-          <li>Вакансии в Jusan</li>
-        </a>
-        {button()}
-      </ol>
+
+      <div style={{ display: "flex" }}>
+        {toggle === true && (
+          <FadeInOut show={toggle} duration={1500}>
+            <ol>
+              <a className={style.menu} href="https://jusan.kz/">
+                <li>Политика конфиденциальности</li>
+              </a>
+              <a className={style.menu} href="https://jusan.kz/">
+                <li>Вакансии в Jusan</li>
+              </a>
+              {button(toggle)}
+            </ol>
+          </FadeInOut>
+        )}
+        {toggle === false && (
+          <ol>
+            <a className={style.menuHidden} href="">
+              <li>Политика конфиденциальности</li>
+            </a>
+            <a className={style.menuHidden} href="">
+              <li>Вакансии в Jusan</li>
+            </a>
+            {button(toggle)}
+          </ol>
+        )}
+
+        <img
+          src={hamburger}
+          style={{ width: "100px" }}
+          onClick={() => {
+            setToggle(!toggle);
+          }}
+        ></img>
+      </div>
     </div>
   );
 };
